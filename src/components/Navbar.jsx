7f6e52788a3312c48e50d253d044c00449a15132
ozkan <ozkandnz09@gmail.com>
2025-02-09 21:24:31 +0300

    navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,26 +7,29 @@ import {
 } from "@headlessui/react";
 import { Link } from "react-router-dom";
 import SwitchButton from "./SwitchButton";
-import avatar from "../assets/icons/avatar.png"
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import avatar from "../assets/icons/avatar.png";
 
 export default function Navbar() {
-  const currentUser = {displayName : "felix franko"}
+  const currentUser = { displayName: "felix franko" };
   // const currentUser = false
   return (
-    <Disclosure as="nav" className="bg-neutral-100 dark:bg-gray-900 dark:text-white fixed top-0 w-full z-20">
+    <Disclosure
+      as="nav"
+      className="bg-neutral-100 dark:bg-gray-900 dark:text-white fixed top-0 w-full z-20"
+    >
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <Link  className="pr-2 text-2xl font-semibold" to="/">Movie App</Link>
+          <Link className="pr-2 text-2xl font-semibold" to="/">
+            Movie App
+          </Link>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {currentUser && (<h5 className="mr-2 capitalize">{currentUser?.displayName}</h5>)}
-            <SwitchButton/>
+            {currentUser && (
+              <h5 className="mr-2 capitalize">{currentUser?.displayName}</h5>
+            )}
+            <SwitchButton />
             {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
+            <Menu as="div" className="relative">
               <div>
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                   <span className="absolute -inset-1.5" />
@@ -43,28 +46,28 @@ export default function Navbar() {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
               >
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    to="/register"
                     className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                   >
-                    Your Profile
-                  </a>
+                    Register
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    to="/login"
                     className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                   >
-                    Settings
-                  </a>
+                    Login
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                  <span
+                    
+                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden cursor-pointer"
                   >
-                    Sign out
-                  </a>
+                    Logout
+                  </span>
                 </MenuItem>
               </MenuItems>
             </Menu>
