a1b48f9ec6a911b5f6542c22c3f82cf55fbe243f
ozkan <ozkandnz09@gmail.com>
2025-02-10 22:43:32 +0300

    customhook

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,11 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext } from 'react'
 
 export const AuthContext = createContext()
+
+//* with custom hook
+export const useAuthContext =()=>{
+    return useContext(AuthContext)
+}
 
 const AuthProvider = () => {
   return (
