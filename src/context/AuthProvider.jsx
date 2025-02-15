304b7e6afe95dbba6f6b469773c6a8032d83cd22
ozkan <ozkandnz09@gmail.com>
2025-02-15 22:56:31 +0300

    sign in

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,7 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { createContext, useContext, useState } from "react";
 import { auth } from "../auth/firebase";
 import { useNavigate } from "react-router-dom";
@@ -14,6 +17,7 @@ export const useAuthContext = () => {
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(false);
   const navigate = useNavigate();
+
   const createUser = async (email, password) => {
     try {
       //? firebase method used to create a new user
@@ -23,10 +27,24 @@ const AuthProvider = ({ children }) => {
         password
       );
       navigate("/login");
-      toastSuccessNotify("Registered succeffully")
-      
+      toastSuccessNotify("Registered succeffully");
     } catch (error) {
-      toastErrorNotify(error.message)
+      toastErrorNotify(error.message);
+    }
+  };
+  
+  const signIn = async (email, password) => {
+    try {
+      //? Firebase method used to log in the current user
+      let userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+      toastSuccessNotify("Logged in  succeffully");
+    } catch (error) {
+      toastErrorNotify(error.message);
     }
   };
 
