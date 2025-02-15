132c185e18e4b8edaa41f97e14d823afb9508279
ozkan <ozkandnz09@gmail.com>
2025-02-15 22:35:20 +0300

    toastify.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
