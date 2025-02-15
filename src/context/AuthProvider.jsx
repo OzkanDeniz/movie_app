2b1b01b9556dee8f0050e8ce12534ad1664896e8
ozkan <ozkandnz09@gmail.com>
2025-02-15 23:24:03 +0300

    userobserver

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -64,10 +64,7 @@ const AuthProvider = ({ children }) => {
 
   const userObserver = () => onAuthStateChanged(auth, (user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      // ...
+      
     } else {
       // User is signed out
       // ...
