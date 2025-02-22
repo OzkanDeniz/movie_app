45576aaa851609dccc05a2e64e699e0e137b70e6
ozkan <ozkandnz09@gmail.com>
2025-02-22 23:01:20 +0300

    usestate

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMovieContext } from "../context/MovieProvider";
 import MovieCard from "../components/MovieCard";
 import Loading from "../components/Loading";
@@ -7,12 +7,18 @@ const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const { movies, loading } = useMovieContext();
   console.log(movies);
   return (
     <>
       <form className="flex justify-center p-2 ">
-        <input type="search" className="w-80 h-8 rounded p-1 m-2 " placeholder="Search a movie..."/>
+        <input
+          type="search"
+          className="w-80 h-8 rounded p-1 m-2 "
+          placeholder="Search a movie..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </form>
       <div className="flex flex-wrap justify-center">
         {loading ? (
