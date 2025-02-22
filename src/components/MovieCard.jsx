a8f01b35e50d95fdfd714b1ed8967f59ae551dc6
ozkan <ozkandnz09@gmail.com>
2025-02-22 22:43:28 +0300

    currnetuser

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { AuthContext, useAuthContext } from "../context/AuthProvider";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
+    const {currentUser} = useAuthContext()
   return (
     <div className="movie">
       <img loading="lazy" src={poster_path ? IMG_API + poster_path : defaultImage} alt="movie-card" />
       <div className="flex align-baseline justify-between p-1 text-white">
         <h5>{title}</h5>
-        <span className="">{vote_average.toFixed(1)}</span>
+        {currentUser && <span className="">{vote_average.toFixed(1)}</span>}
       </div>
       <div className="movie-over">
         <h2>Overview</h2>
