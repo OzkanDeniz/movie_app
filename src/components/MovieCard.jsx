201a910266ef3e38ffcc487f87f535e0f89e25f5
ozkan <ozkandnz09@gmail.com>
2025-02-22 22:29:06 +0300

    class

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,17 +4,17 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const MovieCard = ({ title, poster_path, overview, vote_avarege, id }) => {
+const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
   return (
-    <div>
+    <div className="movie">
       <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="movie-card" />
       <div>
-        <h5>title</h5>
-        <span>vote_average</span>
+        <h5>{title}</h5>
+        <span>{vote_average.toFixed(1)}</span>
       </div>
       <div>
         <h2>Overview</h2>
-        <p>overview</p>
+        <p>{overview}</p>
       </div>
     </div>
   );
