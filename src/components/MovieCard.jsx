af4b096effa2322b30f6a6ecc42205dd50b3dadd
ozkan <ozkandnz09@gmail.com>
2025-02-22 21:39:12 +0300

    img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
-  
+
 const MovieCard = ({ title, poster_path, overview, vote_avarege, id }) => {
   return (
     <div>
-      <img src="" alt="" />
+      <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="movie-card" />
       <div>
         <h5>title</h5>
         <span>vote_average</span>
