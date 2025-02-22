6dca8581f6a05567c354a914bc5c0d45b431a4cc
ozkan <ozkandnz09@gmail.com>
2025-02-22 21:33:07 +0300

    moviecard

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useMovieContext } from "../context/MovieProvider";
+import MovieCard from "../components/MovieCard";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
@@ -11,7 +12,7 @@ const Main = () => {
     <>
       <div>
         {movies.map((movie) => (
-          <p>{movie.title}</p>
+          <MovieCard />
         ))}
       </div>
     </>
