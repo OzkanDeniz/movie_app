b40a6750ee61ab61e51ca4c6b0ff80bb9f9a5f50
ozkan <ozkandnz09@gmail.com>
2025-02-22 21:21:43 +0300

    usemoviecontext

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { useMovieContext } from "../context/MovieProvider";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
-const  SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
-
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const { movies, loading } = useMovieContext();
   return <div>Main</div>;
 };
 
