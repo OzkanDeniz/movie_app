801f0d7c1ba2475f26e73fc889ed20ee658b7f29
ozkan <ozkandnz09@gmail.com>
2025-02-22 21:31:45 +0300

    card style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const MovieCard = () => {
   return (
     <div>
-      
+      <img src="" alt="" />
+      <div>
+        <h5>title</h5>
+        <span>vote_average</span>
+      </div>
+      <div>
+        <h2>Overview</h2>
+        <p>overview</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
