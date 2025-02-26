10da2801f7b013095e8728bbc08610b741e532cc
ozkan <ozkandnz09@gmail.com>
2025-02-26 23:17:21 +0300

    register login !show

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,22 +50,26 @@ export default function Navbar() {
                   transition
                   className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                 >
-                  <MenuItem>
-                    <Link
-                      to="/register"
-                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                    >
-                      Register
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link
-                      to="/login"
-                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                    >
-                      Login
-                    </Link>
-                  </MenuItem>
+                  {!currentUser && (
+                    <>
+                      <MenuItem>
+                        <Link
+                          to="/register"
+                          className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                        >
+                          Register
+                        </Link>
+                      </MenuItem>
+                      <MenuItem>
+                        <Link
+                          to="/login"
+                          className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                        >
+                          Login
+                        </Link>
+                      </MenuItem>
+                    </>
+                  )}
                   <MenuItem>
                     <span
                       onClick={logOut}
