56bbc1830423167916948f495b21147eef7c5dfc
ozkan <ozkandnz09@gmail.com>
2025-02-26 22:24:08 +0300

    apikey

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -3,6 +3,15 @@ import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
   const {movieId} = useParams()
+  
+
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
+  const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
+  const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+  const videoUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`;
+  
   console.log(movieId)
   return <div>MovieDetail</div>;
 };
