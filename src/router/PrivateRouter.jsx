6d867fef68db03c308f31f469376c9bc86b58a59
ozkan <ozkandnz09@gmail.com>
2025-02-26 22:45:28 +0300

    privaterouter

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useAuthContext } from "../context/AuthProvider";
+import { Navigate, Outlet } from "react-router-dom";
+import Login from "../pages/Login";
+
+const PrivateRouter = () => {
+  const { currentUer } = useAuthContext();
+  return currentUer ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
